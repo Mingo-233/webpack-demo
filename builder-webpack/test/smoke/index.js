8c482aa287968174bfdaecdaecd57dfee23df111
mingo <mingo233@qq.com>
2021-08-25 22:00:15 +0800

    冒烟测试

--- builder-webpack/test/smoke/index.js
+++ builder-webpack/test/smoke/index.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 const rimraf = require('rimraf');
-// const Mocha = require('mocha');
+const Mocha = require('mocha');
 console.log('run');
-
+const mocha = new Mocha({
+  timeout: '10000ms'
+});
 // 改变当前运行目录
 process.chdir(path.join(__dirname, 'template'));
 rimraf('./dist',()=>{
@@ -15,9 +17,16 @@ rimraf('./dist',()=>{
       process.exit(2)
     }
     console.log(stats.toString({
-      color:true,
+      colors:true,
       modules: false,
-      children: false
+      children: false,
+      hash: true,
+      timings: true,
     }));
+
+    console.log('Webpack build success, begin run test.');
+    mocha.addFile(path.join(__dirname, 'html-test.js'));
+    mocha.addFile(path.join(__dirname, 'css-js-test.js'));
+    mocha.run();
   })
 })
